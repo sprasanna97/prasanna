e47b26b06425d2bd6b1c9bc3c2b4ee207d4c458c
sprasanna97 <31242014+sprasanna97@users.noreply.github.com>
2017-08-22 16:11:05 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n;
+printf("Enter an integer");
+scanf("%d", &i);
+remainder = i % 2;
+if (n == 0)
+printf("%d is an even integer\n",i);
+else
+printf("%d is an odd integer\n",i);
+}
+getch()
